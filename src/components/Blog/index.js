79bea3926396deb4d7cb07c2585b049d2f1daf38
alain <alain.santamaria57@gmail.com>
2023-05-11 14:50:29 +0200

    test avec des consoles log pour voir l'ordre dexecution

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -8,6 +8,7 @@ import Header from 'src/components/Header';
 import Posts from 'src/components/Posts';
 import Footer from 'src/components/Footer';
 import NotFound from 'src/components/NotFound';
+import Spinner from 'src/components/Spinner';
 
 // data, styles et utilitaires
 import categoriesData from 'src/data/categories';
@@ -28,34 +29,72 @@ const Blog = () => {
 
   const [isZenMode, setIsZenMode] = useState(false);
 
+  // on prépare une variable d'état pour stocker no articles
+  const [posts, setPosts] = useState([]);
+
+  // on  prépare une variable d'état qui va indiquer
+  // si on est en état de chargement ou NotFound
+  const [loading, setLoading] = useState(false);
+
+  // fonction permettant de récupérer lse articles
+  const loadPosts = () => {
+    console.log('on charge les articles');
+
+    //  On passe en état loading
+    setLoading(true);
+    // on simule la latence d'appel de l'api
+    // en programmant l'execution d'une fonction au bout de seconde
+    setTimeout(
+      // fonction exécuté au bout de XX ms
+      () => {
+        console.log('articles chargé');
+        //  on veut mettre à jour la liste des articles dans le useState
+        //  Pour simuler qu'on a eu la réponse de l'api et qu'on ll'a traité
+        // on fait un nouveau rendu ( qui ne change rien car récup depuis le fichier de données)
+        setPosts(postsData);
+        //  ici on sort de l'état de chargement
+        // on ne va plus afficher le Spinner mais le routes utilisants
+        // les articles qui sont maintenant dans le state
+        setLoading(false);
+      },
+      // temps en millisecondes avant lequel la fonction se déclenche
+      2000,
+    );
+    console.log('fin de la fonction loadPosts');
+  };
+
   // console.log(categoriesData);
   // console.log(postsData);
 
   return (
     <div className="blog">
       <Header categories={categoriesData} zenMode={isZenMode} setZenMode={setIsZenMode} />
-      <Routes>
-        {categoriesData.map(
-          (category) => (
-            <Route
-              key={category.route}
-              path={category.route}
-              element={(
-                <Posts
-                  posts={getPostsByCategory(postsData, category.label)}
-                  zenMode={isZenMode}
-                />
-              )}
-            />
-          ),
-        )}
-        {/* le composant permet d'établir une redirection vers un autre chemin */}
-        <Route path="/jquery" element={<Navigate to="/autre" />} />
+      <button type="button" onClick={loadPosts}>Récupérer les articles</button>
+      {loading && <Spinner />}
+      {!loading && (
+        <Routes>
+          {categoriesData.map(
+            (category) => (
+              <Route
+                key={category.route}
+                path={category.route}
+                element={(
+                  <Posts
+                    posts={getPostsByCategory(posts, category.label)}
+                    zenMode={isZenMode}
+                  />
+                )}
+              />
+            ),
+          )}
+          {/* le composant permet d'établir une redirection vers un autre chemin */}
+          <Route path="/jquery" element={<Navigate to="/autre" />} />
 
-        {/* on crée une route qui va afficher une 404
-        si aucune corespondance entre une URL demandée et une route définit n'est déjà trouvé */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+          {/* on crée une route qui va afficher une 404
+          si aucune corespondance entre une URL demandée et une route définit n'est déjà trouvé */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      )}
       <Footer />
     </div>
   );
