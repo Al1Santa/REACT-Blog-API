194afadb0141d2a0e3fcc8323c3cdf8e44edf158
alain <alain.santamaria57@gmail.com>
2023-05-11 17:38:21 +0200

    nettoyage du code de l'api avec DOMpurify, et affinage du nettoyage avec les balise strong et em

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,15 +1,26 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable react/function-component-definition */
+import DOMPurify from 'dompurify';
 import PropTypes from 'prop-types';
 import './styles.scss';
 
-const Post = ({ title, category, excerpt}) => (
-  <article className="post">
-    <h2 className="post-title">{title}</h2>
-    <div className="post-category">{category}</div>
-    <p className="post-excerpt">{excerpt}</p>
-  </article>
-);
+const Post = ({ title, category, excerpt }) => {
+  // On va demandé a DOMpurify de nettoyer le HTML reçu depuis l'API
+  // on a la possibilité d'affiner notre nettoyage en spécifiant
+  // les balises et ou attributs autorisé, le reste des balises ne sera pas interpréter
+  const cleanExcerpt = DOMPurify.sanitize(excerpt, { ALLOWED_TAGS: ['strong', 'em'] });
+  function createMarkup() {
+    return { __html: cleanExcerpt };
+  }
+
+  return (
+    <article className="post">
+      <h2 className="post-title">{title}</h2>
+      <div className="post-category">{category}</div>
+      <p className="post-excerpt" dangerouslySetInnerHTML={createMarkup()} />
+    </article>
+  );
+};
 
 Post.propTypes = {
   category: PropTypes.string.isRequired,
